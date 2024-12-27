8eeab5a97e4e2dbc6e1f107a4a0ae05eb1354e30
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-12-27 21:28:32 +1000

    🎉 Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using OpenAI;
+using System.ClientModel;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var modelId = "Phi-3.5-mini-instruct";
+var uri = "https://models.inference.ai.azure.com";
+var githubPAT = config["GH_PAT"];
+
+// create client
+var client = new OpenAIClient(new ApiKeyCredential(githubPAT), new OpenAIClientOptions { Endpoint = new Uri(uri) });
+
+// Create a chat completion service
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(modelId, client);
+
+// Get the chat completion service
+Kernel kernel = builder.Build();
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory();
+history.AddSystemMessage("You are a useful chatbot. If you don't know an answer, say 'I don't know!'. Always reply in a funny way. Use emojis if possible.");
+
+while (true)
+{
+    Console.Write("Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+    history.AddUserMessage(userQ);
+
+    var sb = new StringBuilder();
+    var result = chat.GetStreamingChatMessageContentsAsync(history);
+    Console.Write("AI: ");
+    await foreach (var item in result)
+    {
+        sb.Append(item);
+        Console.Write(item.Content);
+    }
+    Console.WriteLine();
+
+    history.AddAssistantMessage(sb.ToString());
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# .NET Semantic Kernel & GitHub Models
+
+Sample code for the .NET Semantic Kernel and GitHub Models.
+
+## Resources
+
+https://devblogs.microsoft.com/dotnet/github-ai-models-dotnet-semantic-kernel/
